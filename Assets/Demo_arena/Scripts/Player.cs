b8fef44d42fb01334736642fe4cb3a3e9861d864
MrACLi <mracli@qq.com>
2019-04-23 20:17:30 +0800

    添加人物基本移动
    实现简单人物移动以及跳跃
    在空中暂时默认不允许移动

---
+++ Assets/Demo_arena/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody m_Rigidbody;
+    private bool isGrounded;
+    [SerializeField]private float checkDistance;
+
+    public float jumpPower = 10.0f;
+    public float moveSpeed = 7.0f;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 move, bool jump)
+    {
+        if (move.magnitude > 1) move.Normalize();
+        CheckIsGrounded();
+        if (isGrounded)
+        {
+            HandleSurfaceMove(move, jump);
+        }
+        else
+        {
+            HandleMidairMove();
+        }
+
+    }
+
+    private void CheckIsGrounded()
+    {
+        // RaycastHit hitinfo;
+        if(Physics.Raycast(transform.position, Vector3.down, checkDistance))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void HandleMidairMove()
+    {
+        m_Rigidbody.AddForce(Physics.gravity);
+    }
+
+    private void HandleSurfaceMove(Vector3 move, bool jump)
+    {
+        if (jump)
+        {
+            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, jumpPower, m_Rigidbody.velocity.z);
+        }
+        else
+        {
+            m_Rigidbody.velocity = move * moveSpeed;
+        }
+    }
+
+}
---
+++ Assets/Demo_arena/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform m_Cam;
+    [SerializeField] private Vector3 m_CamForward;
+    [SerializeField] private Vector3 m_Move;
+
+    private bool pressedJump;
+
+    private Player m_Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Player = GetComponent<Player>();
+        // 获取MainCamera用来作为参考控制Player
+        if(Camera.main != null)
+        {
+            m_Cam = Camera.main.transform;
+        }else
+        {
+            Debug.Log("No main camera(Tagged as MainCamera) in use, controller will use world coordinate");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!pressedJump)
+            pressedJump = Input.GetKeyDown(KeyCode.Space);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if(m_Cam != null)
+        {
+            m_CamForward = Vector3.ProjectOnPlane(m_Cam.forward, new Vector3(0, 1, 0));
+            m_Move = m_CamForward * v + m_Cam.right * h;
+        }
+        else
+        {
+            m_Move = Vector3.forward * v + Vector3.right * h;
+        }
+        m_Player.Move(m_Move, pressedJump);
+        pressedJump = false;
+    }
+}
